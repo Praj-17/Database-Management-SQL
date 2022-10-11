ccc5c04f6b4f036f921fc0719623159776183411
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2022-10-12 01:56:07 +0530

    added fnf letter

--- Flexiele/Letters/Pay Slip/payslip.sql
+++ Flexiele/Letters/Pay Slip/payslip.sql
@@ -1,7 +1,8 @@
 SELECT
--- DATE_FORMAT({{month}}, '%M %Y') as 'month2',
+DATE_FORMAT('{{month}}', '%M %Y') as 'month2',
   es.a5 'emp_code',
   es.a4 'emp_name',
+  es.a3 'emp_id',
   DATE_FORMAT(e.a5, '%d-%b-%y') 'doj',
   org_dsg.a3 'designation',
   org_dp.a3 'department',
@@ -29,7 +30,7 @@ FROM
   JOIN fe_hrt_emp_job_d j
     ON (j.a3 = e.a1 and j.cl = e.cl)
     JOIN fe_pyt_emp_bt_t bt
-        ON (bt.a2 = e.a1 )
+        ON (bt.a2 = e.a1 AND bt.a3 = '{{month}}')
 JOIN fe_pyt_emp_payday_summary_t ps
         ON (ps.a2 = j.a3
             AND bt.a3 = ps.a4)
@@ -48,10 +49,10 @@ JOIN fe_pyt_emp_payday_summary_t ps
   LEFT JOIN fe_org_unit_m org_dsg
     ON (j.ou10 = org_dsg.a1)
 WHERE 1 = 1 
-    AND p.cl = 7
---     and e.a1  = {{emp_id}}
---     AND bt.a3 = {{month}}
-    AND e.a11 NOT LIKE '%flexiele%'
+    AND p.cl = {session.clientId}
+    and e.a1  = {{emp_id}}
+    AND bt.a3 = '{{month}}'
+   --  AND e.a11 NOT LIKE '%flexiele%'
     AND LAST_DAY(
         STR_TO_DATE(CONCAT(bt.a3, '-01'), '%Y-%m-%d')
     ) BETWEEN j.from
